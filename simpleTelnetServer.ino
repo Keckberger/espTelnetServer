629792b62fcb158cc9636efc6bb015a78c4dfc44
keda <keckeis.d@gmail.com>
2023-08-05 10:55:10 +0200

    added descriptions

--- simpleTelnetServer.ino
+++ simpleTelnetServer.ino
@@ -1,20 +1,19 @@
 #include <ESP8266WiFi.h>
 
-char ssid[] = "Vonkeck";
-char pass[] = "Zimtgemahlen";
-char clientData[255];
+//setup your WIFI Credentials here
+char ssid[] = "***";
+char pass[] = "***";
 
-WiFiServer server(23);
+
+WiFiServer server(23); //23 is the default port the server is listening to
 static String receivedString[9]; //define how many commands your programm will have
 
-bool checkCommand(char c, const char* command, int commandIndex) {
-  if (c != '\r' && c != '\n') {
-  // Create a static buffer to accumulate incoming characters
-  
-  receivedString[commandIndex] += c;
+bool checkCommand(char clientData, const char* command, int commandIndex) {
+  if (clientData != '\r' && clientData != '\n') { //ignore return or new line   
+  receivedString[commandIndex] += clientData;  //put the recieved input data in a String. Each command has its own "place" in the array
 
   // Check if the receivedString contains the command
-  if (receivedString[commandIndex].indexOf(command) != -1) {
+  if (receivedString[commandIndex].indexOf(command) != -1) { //check the array if command is at some place, it will return -1 if there is no command like the user wrote.
     // Command found, clear the receivedString after processing
     receivedString[commandIndex] = "";
     return true;
@@ -45,7 +44,6 @@ void setup()
   server.begin();
   server.setNoDelay(true);
 
-
 }
 
 void loop() {
@@ -53,25 +51,22 @@ void loop() {
   if(client)
   {
     Serial.println("client connected");
-    while(client.connected()){
-      if(client.available()){
-        char c = client.read();
-        Serial.write(c);
+    while(client.connected()){ //this is true when a client is connected
+      if(client.available()){ //this is true when the client object holds data transmitted by the user.
+        char clientData = client.read();
+        Serial.write(clientData);
         
           // Check for commands
-        if (checkCommand(c, "input1", 1)) {
+        if (checkCommand(clientData, "input1", 1)) {
           // Command "input1" found, do something here
           Serial.println("Received 'input1' command!");
-        } else if (checkCommand(c, "input2",2)) {
+        } else if (checkCommand(clientData, "input2",2)) {
           // Command "input2" found, do something here
           Serial.println("Received 'input2' command!");
-        } else if (checkCommand(c, "input3",3)) {
+        } else if (checkCommand(clientData, "input3",3)) {
           // Command "input3" found, do something here
           Serial.println("Received 'input3' command!");
         }
-        
-
-
       }
     }
     
